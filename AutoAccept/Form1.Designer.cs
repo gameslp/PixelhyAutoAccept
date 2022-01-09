9b5332cb781c001edd789ecfd05bd996a3302e5c
gameslp <maciekciesielski8@gmail.com>
2022-01-09 13:50:36 +0100

    Add files via upload

---
+++ AutoAccept/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace AutoAccept
+{
+    partial class PixelhyAutoAccept
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PixelhyAutoAccept));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.siticoneButton1 = new Siticone.Desktop.UI.WinForms.SiticoneButton();
+            this.siticoneCheckBox1 = new Siticone.Desktop.UI.WinForms.SiticoneCheckBox();
+            this.siticoneCheckBox2 = new Siticone.Desktop.UI.WinForms.SiticoneCheckBox();
+            this.siticoneCheckBox3 = new Siticone.Desktop.UI.WinForms.SiticoneCheckBox();
+            this.siticoneCheckBox4 = new Siticone.Desktop.UI.WinForms.SiticoneCheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Fira Code", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(38, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pixelhy Auto Accept";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label4.Location = new System.Drawing.Point(284, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "by Maciek";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // siticoneButton1
+            // 
+            this.siticoneButton1.AutoRoundedCorners = true;
+            this.siticoneButton1.BorderRadius = 14;
+            this.siticoneButton1.CheckedState.Parent = this.siticoneButton1;
+            this.siticoneButton1.CustomImages.Parent = this.siticoneButton1;
+            this.siticoneButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.siticoneButton1.DisabledState.FillColor = System.Drawing.Color.Gray;
+            this.siticoneButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.siticoneButton1.DisabledState.Parent = this.siticoneButton1;
+            this.siticoneButton1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.siticoneButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.siticoneButton1.ForeColor = System.Drawing.Color.White;
+            this.siticoneButton1.HoverState.Parent = this.siticoneButton1;
+            this.siticoneButton1.Location = new System.Drawing.Point(29, 152);
+            this.siticoneButton1.Name = "siticoneButton1";
+            this.siticoneButton1.ShadowDecoration.Parent = this.siticoneButton1;
+            this.siticoneButton1.Size = new System.Drawing.Size(335, 30);
+            this.siticoneButton1.TabIndex = 13;
+            this.siticoneButton1.Text = "enable";
+            this.siticoneButton1.Click += new System.EventHandler(this.siticoneButton1_Click);
+            // 
+            // siticoneCheckBox1
+            // 
+            this.siticoneCheckBox1.AutoSize = true;
+            this.siticoneCheckBox1.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneCheckBox1.CheckedState.BorderRadius = 0;
+            this.siticoneCheckBox1.CheckedState.BorderThickness = 0;
+            this.siticoneCheckBox1.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneCheckBox1.Location = new System.Drawing.Point(43, 76);
+            this.siticoneCheckBox1.Name = "siticoneCheckBox1";
+            this.siticoneCheckBox1.Size = new System.Drawing.Size(79, 17);
+            this.siticoneCheckBox1.TabIndex = 14;
+            this.siticoneCheckBox1.Text = "1920x1080";
+            this.siticoneCheckBox1.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.siticoneCheckBox1.UncheckedState.BorderRadius = 0;
+            this.siticoneCheckBox1.UncheckedState.BorderThickness = 0;
+            this.siticoneCheckBox1.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.siticoneCheckBox1.CheckedChanged += new System.EventHandler(this.siticoneCheckBox1_CheckedChanged);
+            // 
+            // siticoneCheckBox2
+            // 
+            this.siticoneCheckBox2.AutoSize = true;
+            this.siticoneCheckBox2.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneCheckBox2.CheckedState.BorderRadius = 0;
+            this.siticoneCheckBox2.CheckedState.BorderThickness = 0;
+            this.siticoneCheckBox2.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneCheckBox2.Location = new System.Drawing.Point(43, 118);
+            this.siticoneCheckBox2.Name = "siticoneCheckBox2";
+            this.siticoneCheckBox2.Size = new System.Drawing.Size(79, 17);
+            this.siticoneCheckBox2.TabIndex = 15;
+            this.siticoneCheckBox2.Text = "1920x1080";
+            this.siticoneCheckBox2.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.siticoneCheckBox2.UncheckedState.BorderRadius = 0;
+            this.siticoneCheckBox2.UncheckedState.BorderThickness = 0;
+            this.siticoneCheckBox2.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.siticoneCheckBox2.CheckedChanged += new System.EventHandler(this.siticoneCheckBox2_CheckedChanged);
+            // 
+            // siticoneCheckBox3
+            // 
+            this.siticoneCheckBox3.AutoSize = true;
+            this.siticoneCheckBox3.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneCheckBox3.CheckedState.BorderRadius = 0;
+            this.siticoneCheckBox3.CheckedState.BorderThickness = 0;
+            this.siticoneCheckBox3.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneCheckBox3.Location = new System.Drawing.Point(235, 76);
+            this.siticoneCheckBox3.Name = "siticoneCheckBox3";
+            this.siticoneCheckBox3.Size = new System.Drawing.Size(79, 17);
+            this.siticoneCheckBox3.TabIndex = 16;
+            this.siticoneCheckBox3.Text = "1920x1080";
+            this.siticoneCheckBox3.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.siticoneCheckBox3.UncheckedState.BorderRadius = 0;
+            this.siticoneCheckBox3.UncheckedState.BorderThickness = 0;
+            this.siticoneCheckBox3.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.siticoneCheckBox3.CheckedChanged += new System.EventHandler(this.siticoneCheckBox3_CheckedChanged);
+            // 
+            // siticoneCheckBox4
+            // 
+            this.siticoneCheckBox4.AutoSize = true;
+            this.siticoneCheckBox4.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneCheckBox4.CheckedState.BorderRadius = 0;
+            this.siticoneCheckBox4.CheckedState.BorderThickness = 0;
+            this.siticoneCheckBox4.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.siticoneCheckBox4.Location = new System.Drawing.Point(235, 118);
+            this.siticoneCheckBox4.Name = "siticoneCheckBox4";
+            this.siticoneCheckBox4.Size = new System.Drawing.Size(79, 17);
+            this.siticoneCheckBox4.TabIndex = 17;
+            this.siticoneCheckBox4.Text = "1920x1080";
+            this.siticoneCheckBox4.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.siticoneCheckBox4.UncheckedState.BorderRadius = 0;
+            this.siticoneCheckBox4.UncheckedState.BorderThickness = 0;
+            this.siticoneCheckBox4.UncheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.siticoneCheckBox4.CheckedChanged += new System.EventHandler(this.siticoneCheckBox4_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(68, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "1920x1080";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(68, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "1600x900";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label5.Location = new System.Drawing.Point(259, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 16);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "1280x720";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Fira Code", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label6.Location = new System.Drawing.Point(259, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 16);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "1024x576";
+            // 
+            // PixelhyAutoAccept
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(388, 222);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.siticoneCheckBox4);
+            this.Controls.Add(this.siticoneCheckBox3);
+            this.Controls.Add(this.siticoneCheckBox2);
+            this.Controls.Add(this.siticoneCheckBox1);
+            this.Controls.Add(this.siticoneButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PixelhyAutoAccept";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.PixelhyAutoAccept_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private Siticone.Desktop.UI.WinForms.SiticoneButton siticoneButton1;
+        private Siticone.Desktop.UI.WinForms.SiticoneCheckBox siticoneCheckBox1;
+        private Siticone.Desktop.UI.WinForms.SiticoneCheckBox siticoneCheckBox2;
+        private Siticone.Desktop.UI.WinForms.SiticoneCheckBox siticoneCheckBox3;
+        private Siticone.Desktop.UI.WinForms.SiticoneCheckBox siticoneCheckBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ AutoAccept/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoItX3Lib;
+
+namespace AutoAccept
+{
+    public partial class PixelhyAutoAccept : Form
+    {
+        IntPtr handle;
+        string WINDOW_NAME = "League of Legends";
+
+        AutoItX3 au3 = new AutoItX3();
+
+        public RECT rect;
+        public struct RECT
+        {
+            public int left, top, right, bottom;
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string IpClassName, string IpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern bool GetWindowRect(IntPtr hwnd, out RECT IpRect);
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        int col;
+
+        public PixelhyAutoAccept()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+        }
+
+        private void PixelhyAutoAccept_Load(object sender, EventArgs e)
+        {
+            Thread AA = new Thread(auto_accept) { IsBackground = true };
+            AA.Start();
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void auto_accept()
+        {
+            while (true)
+            {
+                if (siticoneButton1.Text.Contains("disable"))
+                {
+                    getwindowrectangle();
+                    if (siticoneCheckBox1.Checked == true)
+                    {
+                        col = au3.PixelGetColor(rect.left + 860, rect.top + 827);
+                    }else if (siticoneCheckBox2.Checked == true)
+                    {
+                        col = au3.PixelGetColor(rect.left + 725, rect.top + 697);
+                    }
+                    else if (siticoneCheckBox3.Checked == true)
+                    {
+                        col = au3.PixelGetColor(rect.left + 584, rect.top + 555);
+                    }
+                    else if (siticoneCheckBox4.Checked == true)
+                    {
+                        col = au3.PixelGetColor(rect.left + 465, rect.top + 445);
+                    }
+
+                    if (col == 0x1E252A)
+                    {
+                        Thread.Sleep(20);
+                        if (siticoneCheckBox1.Checked == true)
+                        {
+                            au3.MouseMove(rect.left + 860, rect.top + 827, 1);
+                        }
+                        else if (siticoneCheckBox2.Checked == true)
+                        {
+                            au3.MouseMove(rect.left + 725, rect.top + 697, 1);
+                        }
+                        else if (siticoneCheckBox3.Checked == true)
+                        {
+                            au3.MouseMove(rect.left + 584, rect.top + 555, 1);
+                        }
+                        else if (siticoneCheckBox4.Checked == true)
+                        {
+                            au3.MouseMove(rect.left + 465, rect.top + 445, 1);
+                        }
+                        au3.MouseClick("LEFT");
+                        Thread.Sleep(20);
+                        au3.MouseClick("LEFT");
+                        Thread.Sleep(5000);
+                    }
+                }
+                Thread.Sleep(20);
+            }
+        }
+
+        void getwindowrectangle()
+            {
+                handle = FindWindow(null, WINDOW_NAME);
+                GetWindowRect(handle, out rect);
+            }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void siticoneButton1_Click(object sender, EventArgs e)
+        {
+            if (siticoneButton1.Text.Contains("enable"))
+            {
+                siticoneCheckBox1.Enabled = false;
+                siticoneCheckBox2.Enabled = false;
+                siticoneCheckBox3.Enabled = false;
+                siticoneCheckBox4.Enabled = false;
+                siticoneButton1.Text = "disable";
+            }else if (siticoneButton1.Text.Contains("disable"))
+            {
+                siticoneCheckBox1.Enabled = true;
+                siticoneCheckBox2.Enabled = true;
+                siticoneCheckBox3.Enabled = true;
+                siticoneCheckBox4.Enabled = true;
+                siticoneButton1.Text = "enable";
+            }
+        }
+
+        private void siticoneCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (siticoneCheckBox1.Checked == true)
+            {
+                siticoneCheckBox2.Checked = false;
+                siticoneCheckBox3.Checked = false;
+                siticoneCheckBox4.Checked = false;
+            }
+        }
+
+        private void siticoneCheckBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (siticoneCheckBox2.Checked == true)
+            {
+                siticoneCheckBox1.Checked = false;
+                siticoneCheckBox3.Checked = false;
+                siticoneCheckBox4.Checked = false;
+            }
+        }
+
+        private void siticoneCheckBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (siticoneCheckBox3.Checked == true)
+            {
+                siticoneCheckBox1.Checked = false;
+                siticoneCheckBox2.Checked = false;
+                siticoneCheckBox4.Checked = false;
+            }
+        }
+
+        private void siticoneCheckBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (siticoneCheckBox4.Checked == true)
+            {
+                siticoneCheckBox1.Checked = false;
+                siticoneCheckBox2.Checked = false;
+                siticoneCheckBox3.Checked = false;
+            }
+        }
+    }
+}
